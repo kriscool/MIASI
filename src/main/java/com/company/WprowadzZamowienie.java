6c5e4b2a48e3a7e3fd4f088b3110a25dac26e2a0
1Chacko <kuba.olczak.jo@gmail.com>
2018-05-22 18:34:16 +0200

    Implement project skieleton

--- src/main/java/com/company/WprowadzZamowienie.java
+++ src/main/java/com/company/WprowadzZamowienie.java
@@ -9,5 +9,8 @@ public class WprowadzZamowienie implements JavaDelegate {
 	@Override
 	public void execute(DelegateExecution arg0) throws Exception {
 		arg0.setVariable("email", "jakup@gmail.com");
+		arg0.setVariable("emailCorrect", false);
+		arg0.setVariable("available", true);
+		arg0.setVariable("confirmation", false);
 	}
 }
---
+++ src/main/java/com/company/WyswietlBlednaWalidacje.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+public class WyswietlBlednaWalidacje implements JavaDelegate {
+
+	
+	@Override
+	public void execute(DelegateExecution arg0) throws Exception {
+	}
+}
