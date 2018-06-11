ff51843501c91f23a77b3283a83e91972e74c025
WK <221473@edu.p.lodz.pl>
2018-06-12 01:00:26 +0200

    poprawiona baza

--- src/main/java/com/company/db/DB.java
+++ src/main/java/com/company/db/DB.java
@@ -2,54 +2,60 @@ package com.company.db;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
 
+@Transactional
 public class DB {
 
 	SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-	Session session = sessionFactory.openSession();
+	Session session;
+
+	Session session() {
+		return this.session = sessionFactory.openSession();
+	}
 
 	public void add(Films film) {
-		session.beginTransaction();
+		session().beginTransaction();
 		session.save(film);
 		session.getTransaction().commit();
 		session.close();
 	}
-	
-	public List<Films> getAllFilms(){
-		session.beginTransaction();
+
+	public List<Films> getAllFilms() {
+		session().beginTransaction();
 		List<Films> q = session.createQuery("FROM films").list();
 		session.getTransaction().commit();
 		session.close();
 		return q;
-		
+
 	}
-	
-	public Films getFilm(int id){
-		session.beginTransaction();
+
+	public Films getFilm(int id) {
+		session().beginTransaction();
 		Films q = session.load(Films.class, id);
 		session.getTransaction().commit();
 		session.close();
 		return q;
-		
+
 	}
-	
-	public void updateFilm(Films film){
-		session.beginTransaction();
+
+	public void updateFilm(Films film) {
+
+		session().beginTransaction();
 		session.update(film);
 		session.getTransaction().commit();
 		session.close();
 	}
-	
-	public void deleteFilm(Films film){
-		session.beginTransaction();
+
+	public void deleteFilm(Films film) {
+		session().beginTransaction();
 		session.delete(film);
 		session.getTransaction().commit();
 		session.close();
 	}
-	
-	
+
 }
